6665270137d6baac28f2c3cbbf94130bef696989
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-18 16:58:04 +0100

    Add entities for Declaration and Fraction

---
+++ src/SFA.DAS.MI.Domain/Entities/Declaration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.MI.Domain.Entities
+{
+    public class Declaration
+    {
+        public long Id { get; set; }
+        public string EmpRef { get; set; }
+        public string PayrollYear { get; set; }
+        public int PayrollMonth { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public decimal LevyDueYtd { get; set; }
+        public decimal LevyAllowanceForYear { get; set; }
+        public DateTime? CeasationDate { get; set; }
+    }
+}
---
+++ src/SFA.DAS.MI.Domain/Entities/Fraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.MI.Domain.Entities
+{
+    public class Fraction
+    {
+        public long Id { get; set; }
+        public string EmpRef { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateCalculated { get; set; }
+    }
+}

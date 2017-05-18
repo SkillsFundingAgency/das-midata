4b07cb3313452c43a8daa97fbfdd7752297158d1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-18 11:41:26 +0100

    Implement validator for declarations query

---
+++ src/SFA.DAS.MI.Application.Tests/Queries/GetDeclarationsTests/WhenIValidateTheRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.MI.Application.Queries.GetDeclarations;
+
+namespace SFA.DAS.MI.Application.Tests.Queries.GetDeclarationsTests
+{
+    public class WhenIValidateTheRequest
+    {
+        private GetDeclarationsValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new GetDeclarationsValidator();
+        }
+
+        [Test]
+        public async Task ThenFalseIsReturnedWhenNoFieldsArePopulatedAndTheErrorDictionaryIsPopulated()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetDeclarationsRequest());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string, string>("EmpRef", "EmpRef has not been supplied"), actual.ValidationDictionary);
+        }
+
+        [Test]
+        public async Task ThenTrueIsReturnedWhenAllFieldsArePopulated()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetDeclarationsRequest { EmpRef = "123/ABC" });
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+    }
+}
---
+++ src/SFA.DAS.MI.Application/Queries/GetDeclarations/GetDeclarationsValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.MI.Application.Validation;
+
+namespace SFA.DAS.MI.Application.Queries.GetDeclarations
+{
+    public class GetDeclarationsValidator : IValidator<GetDeclarationsRequest>
+    {
+        public Task<ValidationResult> ValidateAsync(GetDeclarationsRequest item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.EmpRef))
+            {
+                validationResult.AddError(nameof(item.EmpRef));
+            }
+
+            return Task.FromResult(validationResult);
+        }
+    }
+}
